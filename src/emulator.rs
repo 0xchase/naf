b15c9ed4dcdb2701e276d7cc375c65c9764d95be
WittsEnd2 <contact@ragnarsecurity.com>
2020-12-09 18:18:43 -0500

    Made modifications for indexing

--- src/emulator.rs
+++ src/emulator.rs
@@ -84,76 +84,82 @@ impl<'a> Emulator<'a> {
         use LlilInst::*;
         use expression::Expr::*;
         use expression::eval_expression;
+        let indexes: Vec<Index> = self.program.index_at(self.state.addr).expect("No instructions ad address");
+        for index in indexes {
+            let inst = index.inst;
 
-        let inst: Inst = self.program.inst_at(self.state.addr).expect("No such instruction");
+
         
-        //This is where we check which instruction it is and set the state to the necessary values.
-        match inst.llil {
-            SetReg(llil) => {
-                let val = eval_expression(llil.expr, &self.state);
-                self.state.regs.set(llil.reg, val);
-                info!("0x{:x} Set register to 0x{:x}", self.state.addr, val);
-            }
-            SetRegSplit(llil) => {
-                let val = eval_expression(llil.source_expr, &self.state);
-                let high: u32 = (val >> 32) as u32;
-                let low: u32 = val as u32;
-
-                self.state.regs.set(llil.dest_reg_low, low as u64);
-                self.state.regs.set(llil.dest_reg_high, high as u64);
-                info!("0x{:x} Set registers to 0x{:x} and 0x{:x}", self.state.addr, low, high);
-            }
-            Push(llil) => {
-                match llil.expr {
-                    Reg(r) => {
-                        info!("0x{:x} Pushing register {}", self.state.addr, r.name);
-                        self.state.memory.store(self.state.regs.rsp, self.state.regs.get(r.name));
-                    },
-                    _ => info!("0x{:x} Pushing other", self.state.addr),
+        // let inst: Inst = self.program.inst_at(self.state.addr).expect("No such instruction");
+        
+        // This is where we check which instruction it is and set the state to the necessary values.
+            match inst.llil {
+                SetReg(llil) => {
+                    let val = eval_expression(llil.expr, &self.state);
+                    self.state.regs.set(llil.reg, val);
+                    info!("0x{:x} Set register to 0x{:x}", self.state.addr, val);
                 }
-                self.state.regs.rsp -= 8;
-            }
-            If(llil) => {
-                let result: u64 = eval_expression(llil.condition, &self.state);
-                info!("0x{:x} Compare returned 0x{:x}", self.state.addr, result);
-                if result == 0 {
-                    self.state.addr = llil.target_false;
-                    info!(" > Branching false");
-                } else {
-                    self.state.addr = llil.target_true;
-                    info!(" > Branching true");
-                }
-            }
-            Store(llil) => {
-                let val: u64 = eval_expression(llil.source_expr, &self.state);
-                let addr: u64 = eval_expression(llil.dest_mem_expr, &self.state);
+                SetRegSplit(llil) => {
+                    let val = eval_expression(llil.source_expr, &self.state);
+                    let high: u32 = (val >> 32) as u32;
+                    let low: u32 = val as u32;
 
-                self.state.memory.store(addr, val);
-
-                info!("0x{:x} Stored 0x{:x} at 0x{:x}", self.state.addr, val, addr);
-            }
-            Call(llil) => {
-                match llil.target {
-                    Value(v) => {
-                        info!("Calling procedure (unimplemented)");
-                    },
-                    _ => error!("0x{:x} Calling other", self.state.addr),
+                    self.state.regs.set(llil.dest_reg_low, low as u64);
+                    self.state.regs.set(llil.dest_reg_high, high as u64);
+                    info!("0x{:x} Set registers to 0x{:x} and 0x{:x}", self.state.addr, low, high);
                 }
-            }
-            Goto(llil) => {
-                info!("0x{:x} Goto instruction at {}", self.state.addr, llil.target);
-                self.state.index = llil.target as usize - 1;
-            }
-            Jump(llil) => {
-                info!("0x{:x} Jump instructiion at {}", self.state.addr, llil.target);
-                self.state.index = llil.target as usize - 1;
-            }
-            Return(llil) => {
-                info!("0x{:x} Return instruction at {}", self.state.addr, llil.target);
-                self.state.index = llil.target as usize - 1; 
-            }
-            _ => {
-                error!("0x{:x} Unimplemented instruction", self.state.addr);
+                Push(llil) => {
+                    match llil.expr {
+                        Reg(r) => {
+                            info!("0x{:x} Pushing register {}", self.state.addr, r.name);
+                            self.state.memory.store(self.state.regs.rsp, self.state.regs.get(r.name));
+                        },
+                        _ => info!("0x{:x} Pushing other", self.state.addr),
+                    }
+                    self.state.regs.rsp -= 8;
+                }
+                If(llil) => {
+                    let result: u64 = eval_expression(llil.condition, &self.state);
+                    info!("0x{:x} Compare returned 0x{:x}", self.state.addr, result);
+                    if result == 0 {
+                        self.state.addr = llil.target_false;
+                        info!(" > Branching false");
+                    } else {
+                        self.state.addr = llil.target_true;
+                        info!(" > Branching true");
+                    }
+                }
+                Store(llil) => {
+                    let val: u64 = eval_expression(llil.source_expr, &self.state);
+                    let addr: u64 = eval_expression(llil.dest_mem_expr, &self.state);
+
+                    self.state.memory.store(addr, val);
+
+                    info!("0x{:x} Stored 0x{:x} at 0x{:x}", self.state.addr, val, addr);
+                }
+                Call(llil) => {
+                    match llil.target {
+                        Value(v) => {
+                            info!("Calling procedure (unimplemented)");
+                        },
+                        _ => error!("0x{:x} Calling other", self.state.addr),
+                    }
+                }
+                Goto(llil) => {
+                    info!("0x{:x} Goto instruction at {}", self.state.addr, llil.target);
+                    self.state.index = llil.target as usize - 1;
+                }
+                Jump(llil) => {
+                    info!("0x{:x} Jump instructiion at {}", self.state.addr, llil.addr);
+                    self.state.index = llil.addr as usize - 1;
+                }
+                Ret(llil) => {
+                    info!("0x{:x} Return instruction at {}", self.state.addr, llil.addr);
+                    self.state.index = llil.addr as usize - 1; 
+                }
+                _ => {
+                    error!("0x{:x} Unimplemented instruction", self.state.addr);
+                }
             }
         }
 
--- src/program.rs
+++ src/program.rs
@@ -107,6 +107,10 @@ impl<'a> Program<'a> {
         }
         return Err(String::from("Couldn't find block at address"));
     }
+    pub fn num_blocks(&self, addr: u64) -> usize {
+        let blocks = self.bv.basic_blocks_containing(addr);
+        return blocks.len();
+    }
 
     // Gets the next instruction
     pub fn inst_after(&self, addr: u64) -> Result<Inst, String> {
@@ -137,12 +141,43 @@ impl<'a> Program<'a> {
         return Err(String::from("Couldn't find instruction"));
     }
 
-    pub fn index_after(&self, addr: u64) -> Result<Index, String> {
-        
-    }
-    pub fn index_at(&self, addr: u64) -> Result<Index, String> { 
-        
+    // // Gets the next index after the next block at the next index.
+    // pub fn index_after(&self, addr: u64, index: u64) -> Result<Vec<Index>, String> {
+    //     let mut curr_index: u64 = 0;
+    //     let mut instructions = Vec::new();
+    //     if let Ok(block) = self.block_at(addr) {
+    //         let mut found: bool = false;
+    //         for inst in block.llil() {
+    //             if found {
+    //                 let return_index = Index{
+    //                     inst: inst,
+    //                     index: curr_index,
+    //                 };
+    //                 instructions.push(return_index);
+    //             }
+    //         }
+    //     }
+    //     return Err(String::from("Couldn't find instruction index"));
+    // }
+    pub fn index_at(&self, addr: u64) -> Result<Vec<Index>, String> { 
+        let mut curr_index: u64 = 0; 
+        let mut instructions = Vec::new();
+        if let Ok(block) = self.block_at(addr) {
+            for inst in block.llil() {
+                if inst.addr == addr {
 
+                    let return_index = Index {
+                        inst: inst,
+                        index: curr_index,
+                    };
+
+                    instructions.push(return_index);
+                }
+                curr_index = curr_index + 1; 
+            }
+            return Ok(instructions);
+        }
+        return Err(String::from("Could not any instructions at address"));
     }
 
     pub fn name(&self) -> String {
@@ -231,7 +266,215 @@ impl<'a> Block<'a> {
                         index += 1;
                     }
                 }
-            /*
+
+            }
+        }
+        return vec;
+    }
+}
+
+// Rust bindings for diassemblly not working 
+pub fn build_inst(inst: binja::llil::Instruction<binja::architecture::CoreArchitecture, binja::llil::Finalized, binja::llil::NonSSA<binja::llil::RegularNonSSA>>) -> Inst {
+    match inst.info() {
+        SetReg(op) => {
+            Inst {
+                addr: op.address(),
+                llil: LlilInst::SetReg(SetReg {reg: format!("{:?}", op.dest_reg()), expr: expression::build_expression(&op.source_expr())}),
+                disass: String::from("mov eax, eax"),
+            }
+        },
+        SetRegSplit(op) =>
+            Inst {
+                addr: op.address(),
+                llil: LlilInst::SetRegSplit(SetRegSplit {dest_reg_high: format!("{:?}", op.dest_reg_high()), dest_reg_low: format!("{:?}", op.dest_reg_low()), source_expr: expression::build_expression(&op.source_expr())}),
+                disass: String::from("mov eax, eax"),
+            },
+        SetFlag(op) =>
+            Inst {
+                addr: op.address(),
+                llil: LlilInst::SetFlag(SetFlag {addr: 5}),
+                disass: String::from("mov eax, eax"),
+            },
+        Store(op) =>
+            Inst {
+                addr: op.address(),
+                llil: LlilInst::Store(Store {source_expr: expression::build_expression(&op.source_expr()), dest_mem_expr: expression::build_expression(&op.dest_mem_expr())}),
+                disass: String::from("mov eax, eax"),
+            },
+        Push(op) =>
+            Inst {
+                addr: op.address(),
+                llil: LlilInst::Push(Push {expr: expression::build_expression(&op.operand())}),
+                disass: String::from("mov eax, eax"),
+            },
+        Jump(op) =>
+            Inst {
+                addr: op.address(),
+                llil: LlilInst::Jump(Jump {addr: 5}),
+                disass: String::from("mov eax, eax"),
+            },
+        JumpTo(op) =>
+            Inst {
+                addr: op.address(),
+                llil: LlilInst::JumpTo(JumpTo {addr: 5}),
+                disass: String::from("mov eax, eax"),
+            },
+        Call(op) =>
+            Inst {
+                addr: op.address(),
+                llil: LlilInst::Call(Call {target: expression::build_expression(&op.target())}),
+                disass: String::from("mov eax, eax"),
+            },
+        Ret(op) =>
+            Inst {
+                addr: op.address(),
+                llil: LlilInst::Ret(Ret {addr: 5}),
+                disass: String::from("mov eax, eax"),
+            },
+        If(op) =>
+            Inst {
+                addr: op.address(),
+                llil: LlilInst::If(If {condition: expression::build_expression(&op.condition()), target_true: 0x40082b, target_false: 0x400817}),
+                disass: String::from("mov eax, eax"),
+            },
+        Nop(op) => 
+            Inst {
+                addr: op.address(),
+                llil: LlilInst::Nop(),
+                disass: String::from("mov eax, eax"),
+            },
+        NoRet(op) =>
+            Inst {
+                addr: op.address(),
+                llil: LlilInst::NoRet(),
+                disass: String::from("mov eax, eax"),
+            },
+        Goto(op) =>
+            Inst {
+                addr: op.address(),
+                llil: LlilInst::Goto(Goto {target: 115}),
+                disass: String::from("mov eax, eax"),
+            },
+        Syscall(op) =>
+            Inst {
+                addr: op.address(),
+                llil: LlilInst::Syscall(),
+                disass: String::from("mov eax, eax"),
+            },
+        Bp(op) =>
+            Inst {
+                addr: op.address(),
+                llil: LlilInst::Bp(),
+                disass: String::from("mov eax, eax"),
+            },
+        Trap(op) =>
+            Inst {
+                addr: op.address(),
+                llil: LlilInst::Trap(),
+                disass: String::from("mov eax, eax"),
+            },
+        Undef(op) =>
+            Inst {
+                addr: op.address(),
+                llil: LlilInst::Undef(),
+                disass: String::from("mov eax, eax"),
+            },
+        _ =>
+            Inst {
+                addr: 0,
+                llil: LlilInst::Undef(),
+                disass: String::from("mov eax, eax"),
+            },
+    }
+}
+
+pub struct Inst {
+    pub addr: u64,
+    pub llil: LlilInst,
+    pub disass: String,
+}
+
+pub struct Index {
+    pub inst: Inst, 
+    pub index: u64
+}
+
+pub enum LlilInst {
+    SetReg(SetReg),
+    SetRegSplit(SetRegSplit),
+    SetFlag(SetFlag),
+    Store(Store),
+    Push(Push),
+    Jump(Jump),
+    JumpTo(JumpTo),
+    Call(Call),
+    Ret(Ret),
+    If(If),
+    Nop(),
+    NoRet(),
+    Goto(Goto),
+    Syscall(),
+    Bp(),
+    Trap(),
+    Undef(),
+}
+
+pub struct SetReg {
+    pub expr: expression::Expr,
+    pub reg: String,
+}
+
+pub struct SetRegSplit {
+    pub dest_reg_high: String,
+    pub dest_reg_low: String,
+    pub source_expr: expression::Expr,
+}
+
+pub struct SetFlag {
+    pub addr: u64,
+}
+
+pub struct Store {
+    pub dest_mem_expr: expression::Expr,
+    pub source_expr: expression::Expr,
+}
+
+pub struct Push {
+    pub expr: expression::Expr,
+}
+
+pub struct Jump {
+    pub addr: u64,
+}
+
+pub struct JumpTo {
+    pub addr: u64,
+}
+
+pub struct Call {
+    pub target: expression::Expr,
+}
+
+pub struct Ret {
+    pub addr: u64,
+}
+
+pub struct If {
+    pub condition: expression::Expr,
+    pub target_true: u64,
+    pub target_false: u64,
+}
+
+pub struct Goto {
+    pub target: u64,
+}
+
+pub struct Return {
+    pub target: u64,
+}
+
+// Found in llil()
+           /*
                 for llil_block in llil.basic_blocks().into_iter() {
                     //info!(" > Getting block for llil");
 
@@ -401,203 +644,4 @@ impl<'a> Block<'a> {
                     */
                     }
                 */
-            }
-        }
-        return vec;
-    }
-}
-
-pub fn build_inst(inst: binja::llil::Instruction<binja::architecture::CoreArchitecture, binja::llil::Finalized, binja::llil::NonSSA<binja::llil::RegularNonSSA>>) -> Inst {
-    match inst.info() {
-        SetReg(op) => {
-            Inst {
-                addr: op.address(),
-                llil: LlilInst::SetReg(SetReg {reg: format!("{:?}", op.dest_reg()), expr: expression::build_expression(&op.source_expr())}),
-                disass: String::from("mov eax, eax"),
-            }
-        },
-        SetRegSplit(op) =>
-            Inst {
-                addr: op.address(),
-                llil: LlilInst::SetRegSplit(SetRegSplit {dest_reg_high: format!("{:?}", op.dest_reg_high()), dest_reg_low: format!("{:?}", op.dest_reg_low()), source_expr: expression::build_expression(&op.source_expr())}),
-                disass: String::from("mov eax, eax"),
-            },
-        SetFlag(op) =>
-            Inst {
-                addr: op.address(),
-                llil: LlilInst::SetFlag(SetFlag {addr: 5}),
-                disass: String::from("mov eax, eax"),
-            },
-        Store(op) =>
-            Inst {
-                addr: op.address(),
-                llil: LlilInst::Store(Store {source_expr: expression::build_expression(&op.source_expr()), dest_mem_expr: expression::build_expression(&op.dest_mem_expr())}),
-                disass: String::from("mov eax, eax"),
-            },
-        Push(op) =>
-            Inst {
-                addr: op.address(),
-                llil: LlilInst::Push(Push {expr: expression::build_expression(&op.operand())}),
-                disass: String::from("mov eax, eax"),
-            },
-        Jump(op) =>
-            Inst {
-                addr: op.address(),
-                llil: LlilInst::Jump(Jump {addr: 5}),
-                disass: String::from("mov eax, eax"),
-            },
-        JumpTo(op) =>
-            Inst {
-                addr: op.address(),
-                llil: LlilInst::JumpTo(JumpTo {addr: 5}),
-                disass: String::from("mov eax, eax"),
-            },
-        Call(op) =>
-            Inst {
-                addr: op.address(),
-                llil: LlilInst::Call(Call {target: expression::build_expression(&op.target())}),
-                disass: String::from("mov eax, eax"),
-            },
-        Ret(op) =>
-            Inst {
-                addr: op.address(),
-                llil: LlilInst::Ret(Ret {addr: 5}),
-                disass: String::from("mov eax, eax"),
-            },
-        If(op) =>
-            Inst {
-                addr: op.address(),
-                llil: LlilInst::If(If {condition: expression::build_expression(&op.condition()), target_true: 0x40082b, target_false: 0x400817}),
-                disass: String::from("mov eax, eax"),
-            },
-        Nop(op) => 
-            Inst {
-                addr: op.address(),
-                llil: LlilInst::Nop(),
-                disass: String::from("mov eax, eax"),
-            },
-        NoRet(op) =>
-            Inst {
-                addr: op.address(),
-                llil: LlilInst::NoRet(),
-                disass: String::from("mov eax, eax"),
-            },
-        Goto(op) =>
-            Inst {
-                addr: op.address(),
-                llil: LlilInst::Goto(Goto {target: 115}),
-                disass: String::from("mov eax, eax"),
-            },
-        Syscall(op) =>
-            Inst {
-                addr: op.address(),
-                llil: LlilInst::Syscall(),
-                disass: String::from("mov eax, eax"),
-            },
-        Bp(op) =>
-            Inst {
-                addr: op.address(),
-                llil: LlilInst::Bp(),
-                disass: String::from("mov eax, eax"),
-            },
-        Trap(op) =>
-            Inst {
-                addr: op.address(),
-                llil: LlilInst::Trap(),
-                disass: String::from("mov eax, eax"),
-            },
-        Undef(op) =>
-            Inst {
-                addr: op.address(),
-                llil: LlilInst::Undef(),
-                disass: String::from("mov eax, eax"),
-            },
-        _ =>
-            Inst {
-                addr: 0,
-                llil: LlilInst::Undef(),
-                disass: String::from("mov eax, eax"),
-            },
-    }
-}
-
-pub struct Inst {
-    pub addr: u64,
-    pub llil: LlilInst,
-    pub disass: String,
-}
-
-pub enum LlilInst {
-    SetReg(SetReg),
-    SetRegSplit(SetRegSplit),
-    SetFlag(SetFlag),
-    Store(Store),
-    Push(Push),
-    Jump(Jump),
-    JumpTo(JumpTo),
-    Call(Call),
-    Ret(Ret),
-    If(If),
-    Nop(),
-    NoRet(),
-    Goto(Goto),
-    Syscall(),
-    Bp(),
-    Trap(),
-    Undef(),
-}
-
-pub struct SetReg {
-    pub expr: expression::Expr,
-    pub reg: String,
-}
-
-pub struct SetRegSplit {
-    pub dest_reg_high: String,
-    pub dest_reg_low: String,
-    pub source_expr: expression::Expr,
-}
-
-pub struct SetFlag {
-    pub addr: u64,
-}
-
-pub struct Store {
-    pub dest_mem_expr: expression::Expr,
-    pub source_expr: expression::Expr,
-}
-
-pub struct Push {
-    pub expr: expression::Expr,
-}
-
-pub struct Jump {
-    pub addr: u64,
-}
-
-pub struct JumpTo {
-    pub addr: u64,
-}
-
-pub struct Call {
-    pub target: expression::Expr,
-}
-
-pub struct Ret {
-    pub addr: u64,
-}
-
-pub struct If {
-    pub condition: expression::Expr,
-    pub target_true: u64,
-    pub target_false: u64,
-}
-
-pub struct Goto {
-    pub target: u64,
-}
-
-pub struct Return {
-    pub target: u64,
-}
 
